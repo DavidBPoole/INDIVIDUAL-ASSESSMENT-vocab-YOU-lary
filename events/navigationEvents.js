6ca9e59166dbcc92eef223f2834f0463ac484350
David Poole <dbpoole7@gmail.com>
2023-06-07 00:58:59 -0500

    hooked up the navbar create entry button to new word form

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -4,12 +4,18 @@ import {
   getWords, filterJsWords, filterHtmlWords, filterCssWords
 } from '../api/wordData';
 import { showWords } from '../pages/words';
+import addWordForm from '../components/forms/addWordForm';
 
-// navigation events
+// NAVIGTAION EVENTS
 const navigationEvents = (user) => {
   // ALL WORDS ON LOGO
   document.querySelector('#logo').addEventListener('click', () => {
     getWords(user.uid).then(showWords);
+  });
+
+  // CREATE AN ENTRY BUTTON
+  document.querySelector('#createEntry').addEventListener('click', () => {
+    addWordForm({}, user);
   });
 
   // ALL WORDS
