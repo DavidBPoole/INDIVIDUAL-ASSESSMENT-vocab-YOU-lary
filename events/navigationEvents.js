a86706e9c84c03837d88d65e91a387432d8b2d5a
David Poole <dbpoole7@gmail.com>
2023-06-10 13:44:20 -0500

    fixed time issue and time stamps in database when initially created show date but further intrday updates show milliseconds from UTC

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -9,34 +9,40 @@ import addWordForm from '../components/forms/addWordForm';
 // NAVIGTAION EVENTS
 const navigationEvents = (user) => {
   // ALL WORDS ON LOGO
-  document.querySelector('#logo').addEventListener('click', () => {
+  document.querySelector('#logo').addEventListener('click', (e) => {
+    console.warn(e.targetn, 'LOGO/ALL nav link clicked');
     getWords(user.uid).then(showWords);
   });
 
   // CREATE AN ENTRY BUTTON
-  document.querySelector('#createEntry').addEventListener('click', () => {
+  document.querySelector('#createEntry').addEventListener('click', (e) => {
+    console.warn(e.target);
     addWordForm({}, user);
   });
 
   // ALL WORDS
-  document.querySelector('#allWords').addEventListener('click', () => {
+  document.querySelector('#allWords').addEventListener('click', (e) => {
+    console.warn(e.target, 'ALL nav link clicked');
     getWords(user.uid).then(showWords);
   });
 
   // HTML WORDS
-  document.querySelector('#navHtml').addEventListener('click', () => {
+  document.querySelector('#navHtml').addEventListener('click', (e) => {
+    console.warn(e.target, 'HTML nav link clicked');
     // console.warn('CLICKED SALE BOOKS');
     filterHtmlWords(user.uid).then(showWords);
   });
 
   // CSS WORDS
-  document.querySelector('#navCss').addEventListener('click', () => {
+  document.querySelector('#navCss').addEventListener('click', (e) => {
+    console.warn(e.target, 'CSS nav link clicked');
     // console.warn('CLICKED SALE BOOKS');
     filterCssWords(user.uid).then(showWords);
   });
 
   // JAVASCRIPT WORDS
-  document.querySelector('#navJavascript').addEventListener('click', () => {
+  document.querySelector('#navJavascript').addEventListener('click', (e) => {
+    console.warn(e.target, 'JS nav link clicked');
     // console.warn('CLICKED SALE BOOKS');
     filterJsWords(user.uid).then(showWords);
   });
--- utils/timestamp.js
+++ utils/timestamp.js
@@ -1,4 +1,4 @@
-const currentTime = new Date().toLocaleDateString();
+const currentTime = new Date().toISOString();
 const timestamp = currentTime;
 
 export default timestamp;
